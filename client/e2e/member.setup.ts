ecf110ff8c60703a731054fcb9bc1ff67d79290c
ceenobi <charlesmutob@gmail.com>
2026-03-17 15:15:13 +0100

    fixed test issue where placeholder was mismatched

--- client/e2e/member.setup.ts
+++ client/e2e/member.setup.ts
@@ -7,7 +7,7 @@ const authFile = path.join(__dirname, ".auth/member.json");
 
 setup("authenticate as member", async ({ page }) => {
   await page.goto("/account/login");
-  await page.getByPlaceholder("Your email").fill("cobimbachu@gmail.com");
+  await page.getByPlaceholder("Member email").fill("cobimbachu@gmail.com");
   await page.getByPlaceholder("Your password").fill("Techstudio!!1");
   await page.getByRole("button", { name: "Sign In" }).click();
 
